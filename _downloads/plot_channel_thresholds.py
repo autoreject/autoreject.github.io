6395967e51a2de2f1c18516cf9583896b8b5c055
Mainak Jas <mainak.jas@telecom-paristech.fr>
2016-10-04 19:03:23 +0200

    Make install

--- _downloads/plot_channel_thresholds.py
+++ _downloads/plot_channel_thresholds.py
@@ -59,8 +59,8 @@ epochs.pick_types(meg='grad', eeg=False, stim=False, eog=False,
 from autoreject import compute_thresholds
 import numpy as np
 
-# thresh_range = dict(grad=(4e-13, 900e-13))
-threshes = compute_thresholds(epochs, method='random_search')['meg']
+threshes = compute_thresholds(epochs, method='random_search',
+                              random_state=42)['meg']
 
 ###############################################################################
 # Finally, let us plot a histogram of the channel-level thresholds to verify
