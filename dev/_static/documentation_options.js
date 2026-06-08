8f94c730711a0e31ac18578858d1f3fd93d38eeb
Circle Ci <circle@autoreject.com>
2026-06-08 22:26:22 +0000

    doc updates [skip ci]

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.5.0.dev25+g16be71c84',
+    VERSION: '0.5.0.dev26+g29bec2a57',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
